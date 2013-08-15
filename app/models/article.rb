730730f091181585f8d0c223227ddd6270992128
Daniel Blanco Rojas <daniel.blancorojas@gmail.com>
2013-08-15 17:42:23 -0600

    Initializing the money gem and making the article model pass

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,21 @@
+# This class manage Super-Zapatos' Stores.
+#
+# Attributes:
+# => name:string
+# => description:text
 class Article < ActiveRecord::Base
   
   belongs_to :store
+  monetize :price_cents
+  
+  # Validations
+  validates :name, :presence => true, :uniqueness => {:case_sensitive => false}, :length => {:maximum => 100}
+  validates :description, :length => {:maximum => 500}
+  validates :total_in_shelf, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
+  validates :total_in_vault, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
+
+  # Pagination.
+  paginates_per 30
+  max_paginates_per 100
   
 end
