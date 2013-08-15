e444e0cc21661b5e42d1977e78551c557d6aa77a
Daniel Blanco Rojas <daniel.blancorojas@gmail.com>
2013-08-15 09:32:45 -0600

    Adding first commit with default Rails 4 project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SuperZapatos::Application.config.secret_key_base = '8adcc1f4d6b9252419830e4d8d44002ea8782489419ea4e0c89e7393ff2d71ff9f54f0219a6a99da67f388df472ca6276882c641dd8327ef9eedfdf463f23432'
