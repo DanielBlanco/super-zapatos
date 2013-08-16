0592b3326d095e1b049ab767e800a862e00ea626
Daniel Blanco Rojas <daniel.blancorojas@gmail.com>
2013-08-15 22:06:11 -0600

    Fixing the articles routing spec

--- spec/routing/articles_routing_spec.rb
+++ spec/routing/articles_routing_spec.rb
@@ -4,31 +4,31 @@ describe ArticlesController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/articles").should route_to("articles#index")
+      get("/stores/1/articles").should route_to(:controller => "articles", :action => "index", :store_id => '1')
     end
 
     it "routes to #new" do
-      get("/articles/new").should route_to("articles#new")
+      get("/stores/1/articles/new").should route_to(:controller => "articles", :action => "new", :store_id => '1')
     end
 
     it "routes to #show" do
-      get("/articles/1").should route_to("articles#show", :id => "1")
+      get("/stores/1/articles/1").should route_to(:controller => "articles", :action => "show", :store_id => '1', :id => '1')
     end
 
     it "routes to #edit" do
-      get("/articles/1/edit").should route_to("articles#edit", :id => "1")
+      get("/stores/1/articles/1/edit").should route_to(:controller => "articles", :action => "edit", :store_id => '1', :id => '1')
     end
 
     it "routes to #create" do
-      post("/articles").should route_to("articles#create")
+      post("/stores/1/articles").should route_to(:controller => "articles", :action => "create", :store_id => '1')
     end
 
     it "routes to #update" do
-      put("/articles/1").should route_to("articles#update", :id => "1")
+      put("/stores/1/articles/1").should route_to(:controller => "articles", :action => "update", :store_id => '1', :id => '1')
     end
 
     it "routes to #destroy" do
-      delete("/articles/1").should route_to("articles#destroy", :id => "1")
+      delete("/stores/1/articles/1").should route_to(:controller => "articles", :action => "destroy", :store_id => '1', :id => '1')
     end
 
   end
